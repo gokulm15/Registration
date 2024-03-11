de66fe24b5614234e132a77ccc1a0813dad6b7f3
Gokul M <gokulkarthic60@gmail.com>
2024-03-11 12:49:16 +0530

    changes

--- Sources/RegistrationPackage/RegistrationUIView.swift
+++ Sources/RegistrationPackage/RegistrationUIView.swift
@@ -9,20 +9,55 @@ import SwiftUI
 
 struct RegistrationUIView: View {
     @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
 
     var body: some View {
         VStack {
+            Text("Registration Form")
+                .font(.largeTitle)
+                .padding()
+
             TextField("Username", text: $username)
                 .padding()
                 .textFieldStyle(RoundedBorderTextFieldStyle())
 
-            Text("Entered username: \(username)")
+            SecureField("Password", text: $password)
                 .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            SecureField("Confirm Password", text: $confirmPassword)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            Button(action: {
+                // Add your registration logic here
+                self.register()
+            }) {
+                Text("Register")
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+
+            Spacer()
         }
         .padding()
     }
+
+    func register() {
+        // Implement your registration logic here
+        // You can use the values of username, password, and confirmPassword
+        // to perform registration actions, like creating an account, making API calls, etc.
+    }
 }
 
-#Preview {
-    RegistrationUIView()
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationUIView()
+    }
 }
+
